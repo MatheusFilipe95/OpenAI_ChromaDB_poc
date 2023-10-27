47df0eb234d6212657f6cf5a1087c50cf8123235
MatheusFilipe95 <mafieu666@hotmail.com>
2023-10-27 11:08:13 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { OpenAIEmbeddingFunction } = require("chromadb");
+const OpenAI = require("openai");
+
+require('dotenv').config();
+
+const openai = new OpenAI();
+
+const embedder = new OpenAIEmbeddingFunction({
+   openai_api_key: process.env.OPENAI_API_KEY,
+});
+
+async function main() {
+   const completion = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo",
+   });
+
+   console.log(completion.choices[0]);
+}
+
+main();
+
